6a1eeed21d612c6ca6a366a2497a707bb7a1f4e3
ericveganoguera <eveno229@gmail.com>
2023-03-03 11:41:30 +0100

    Add a Game Over Window

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+const displayGameOver = document.getElementById("game-over")
+
 class Player {
   constructor() {
     this.width = 12;
     this.height = 12;
-    this.initialPositionX = 100/2-this.width/2; //Centered
-    this.positionX = this.initialPositionX
+    this.initialPositionX = 100 / 2 - this.width / 2; //Centered
+    this.positionX = this.initialPositionX;
     this.positionY = 0;
     this.speedMovement = 2;
     this.playerElm = document.getElementById("player");
@@ -13,13 +15,13 @@ class Player {
     this.playerElm.style.height = this.height + "vh";
   }
   moveRight() {
-    if (this.positionX<this.initialPositionX*2){
+    if (this.positionX < this.initialPositionX * 2) {
       this.positionX += this.speedMovement;
       this.playerElm.style.left = this.positionX + "vh";
     }
   }
   moveLeft() {
-    if (this.positionX>0) {
+    if (this.positionX > 0) {
       this.positionX -= this.speedMovement;
       this.playerElm.style.left = this.positionX + "vh";
     }
@@ -36,11 +38,10 @@ class Player {
 
 class Enemy {
   constructor() {
-    this.positionX = Math.floor(Math.random() * 20);
-    this.positionX = this.positionX * 5;
-    this.positionY = 100;
     this.width = 6;
     this.height = 6;
+    this.positionX = Math.floor(Math.random() * 20) * 5;
+    this.positionY = 100 + this.width;
     this.speedMovement = 2;
     this.createDomElement();
   }
@@ -57,10 +58,10 @@ class Enemy {
     this.enemySpawn.style.width = this.width + "vh";
     this.enemySpawn.style.height = this.height + "vh";
 
-    if (this.positionY < -10) {
-      game.allEnemies.shift();
-      this.boardElm.removeChild(this.enemySpawn);
-    }
+    // if (this.positionY < -10) {
+    //   game.allEnemies.shift();
+    //   this.boardElm.removeChild(this.enemySpawn);
+    // }
   }
 }
 
@@ -83,13 +84,17 @@ class Game {
       game.allEnemies.push(newEnemy);
     }, 2000);
 
+    setTimeout(() => {
+      const newEnemyFast = new EnemyFast(15);
+      game.allEnemies.push(newEnemyFast);
+    }, 6000);
+
     //Move the enemies down every XX ms
     this.moveEnemy = setInterval(() => {
       game.allEnemies.forEach((element) => {
-        if (!this.pause) {
-          this.detectCollision(element);
-        }
+        this.detectCollision(element);
         element.moveDown();
+        this.removeEnemy(element);
       });
     }, 64);
   }
@@ -130,13 +135,23 @@ class Game {
       element.positionY < this.player.positionY + this.player.height &&
       element.height + element.positionY > this.player.positionY
     ) {
+      displayGameOver.style.display = "flex"
       clearInterval(this.moveEnemy);
       clearInterval(this.spawnEnemy);
     }
   }
+  removeEnemy(enemy) {
+    if (enemy.positionY < 0) {
+      enemy.enemySpawn.remove();
+      this.allEnemies.shift();
+    }
+  }
 }
-
-
+class EnemyFast extends Enemy {
+  constructor(speedMovement) {
+    super(speedMovement);
+  }
+}
 
 const game = new Game();
 game.start();
